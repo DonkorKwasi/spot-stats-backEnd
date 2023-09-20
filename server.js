836b18ab29b2c98b2802cd8df954fb0aba9e6324
kwasi <kwasi.adom.donkor@gmail.com>
2023-09-20 03:37:29 -0400

    First version of back end

---
+++ server.js
@@ -0,0 +1,93 @@
+
+const express = require('express');
+const app = express();
+const cors = require("cors");
+const jwt = require('jsonwebtoken');
+const passport = require("passport");
+const passportJWT = require("passport-jwt");
+const dotenv = require("dotenv");
+dotenv.config();
+
+
+const userService = require("./user-service.js");
+const port = process.env.PORT || 8000
+let ExtractJwt = passportJWT.ExtractJwt;
+let JwtStrategy = passportJWT.Strategy;
+let jwtOptions = {};
+jwtOptions.jwtFromRequest = ExtractJwt.fromAuthHeaderWithScheme('jwt');
+jwtOptions.secretOrKey = process.env.JWT_SECRET 
+let strategy = new JwtStrategy(jwtOptions, function (jwt_payload, next) {
+    console.log('payload received', jwt_payload);
+
+    if (jwt_payload) {
+        // The following will ensure that all routes using
+        // passport.authenticate have a req.user._id, req.user.userName, req.user.fullName & req.user.role values
+        // that matches the request payload data
+        next(null, {
+          _id: jwt_payload._id,
+          userName: jwt_payload.userName,
+        
+        });
+      } else {
+        next(null, false);
+      }
+    });
+    passport.use(strategy);
+app.use(passport.initialize());
+app.use(express.json());
+app.use(cors());
+
+app.post("/api/user/register", (req, res) => {
+  userService.registerUser(req.body)
+  .then((msg) => {
+      res.json({ "message": msg });
+  }).catch((msg) => {
+      res.status(422).json({ "message": msg });
+  });
+});
+app.post("/api/user/login", (req, res) => {
+  userService.checkUser(req.body).then((user) => {
+      console.log(user);
+      let payload = {
+          _id: user._id,
+          userName: user.userName,
+         
+        };
+        let token = jwt.sign(payload, jwtOptions.secretOrKey);
+      res.status(200).json({ "message": "login complete", token :token})
+  }).catch(msg => {
+      res.status(422).json({ error: msg });
+  })
+});
+
+app.get("/api/user/reviews", passport.authenticate('jwt', { session: false }), (req, res) => {
+  userService.getReviews(req.user._id)
+  .then(data => {
+      res.json(data);
+  }).catch(msg => {
+      res.status(422).json({ error: msg });
+  })
+
+});
+
+app.put("/api/user/reviews", passport.authenticate('jwt', { session: false }), (req, res) => {
+  userService.addReview(req.user._id, req.body)
+  .then(data => {
+      res.json(data)
+  }).catch(msg => {
+      res.status(422).json({ error: msg });
+  })
+});
+app.delete("/api/user/reviews/", passport.authenticate('jwt', { session: false }), (req, res) => {
+  userService.removeFavourite(req.user._id, req.body)
+  .then(data => {
+      res.json(data)
+  }).catch(msg => {
+      res.status(422).json({ error: msg });
+  })
+});
+
+
+app.listen(port, () => {
+    console.log(`app listening on port ${port}`)
+  })
---
+++ user-service.js
@@ -0,0 +1,138 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+
+let mongoDBConnectionString = process.env.MONGO_URL;
+
+const reviewSchema = new mongoose.Schema({
+cover: String,
+albumTitle: String,
+title : String,
+perSong: [Boolean],
+score: Number
+
+
+});
+
+let Schema = mongoose.Schema;
+let userSchema = new Schema({
+    userName: {
+        type: String,
+        unique: true
+    },
+    password: String,
+   reviews: [reviewSchema],
+    
+});
+let User;
+module.exports.connect = function () {
+    return new Promise(function (resolve, reject) {
+        let db = mongoose.createConnection(mongoDBConnectionString);
+
+        db.on('error', err => {
+            reject(err);
+        });
+
+        db.once('open', () => {
+            User = db.model("users", userSchema);
+            resolve();
+        });
+    });
+};
+
+module.exports.registerUser = function (userData) {
+    return new Promise(function (resolve, reject) {
+
+        if (userData.password != userData.password2) {
+            reject("Passwords do not match");
+        } else {
+
+            bcrypt.hash(userData.password, 10).then(hash => {
+
+                userData.password = hash;
+
+                let newUser = new User(userData);
+
+                newUser.save().then(() => {
+                    resolve("User " + userData.userName + " successfully registered");  
+                }).catch(err => {
+                    if (err.code == 11000) {
+                        reject("User Name already taken");
+                    } else {
+                        reject("There was an error creating the user: " + err);
+                    }
+                })
+            }).catch(err => reject(err));
+        }
+    });
+}; 
+
+module.exports.checkUser = function (userData) {
+    return new Promise(function (resolve, reject) {
+
+        User.findOne({ userName: userData.userName })
+            .exec()
+            .then(user => {
+                bcrypt.compare(userData.password, user.password).then(res => {
+                    if (res === true) {
+                       resolve(user);
+                    } else {
+                        reject("Incorrect password for user " + userData.userName);
+                    }
+                });
+            }).catch(err => {
+                reject("Unable to find user " + userData.userName);
+            });
+    });
+};
+
+module.exports.getReviews = function (id) {
+    return new Promise(function (resolve, reject) {
+
+        User.findById(id)
+            .exec()
+            .then(user => {
+                resolve(user.reviews)
+            }).catch(err => {
+                reject(`Unable to get favourites for user with id: ${id}`);
+            });
+    });
+}
+
+
+module.exports.addReview = function (id, review) {
+
+    return new Promise(function (resolve, reject) {
+
+        User.findById(id).exec().then(user => {
+            if (user.reviews.length < 50) {
+                User.findByIdAndUpdate(id,
+                    { $addToSet: { reviews: review } },
+                    { new: true }
+                ).exec()
+                    .then(user => { resolve(user.favourites); })
+                    .catch(err => { reject(`Unable to update favourites for user with id: ${id}`); })
+            } else {
+                reject(`Unable to update favourites for user with id: ${id}`);
+            }
+
+        })
+
+    });
+
+
+}
+
+module.exports.removeReview = function (id, review) {
+    return new Promise(function (resolve, reject) {
+        User.findByIdAndUpdate(id,
+            { $pull: { reviews: review } },
+            { new: true }
+        ).exec()
+            .then(user => {
+                resolve(user.reviews);
+            })
+            .catch(err => {
+                reject(`Unable to update favourites for user with id: ${id}`);
+            })
+    });
+}
